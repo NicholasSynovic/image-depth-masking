edb3ecdd97437775a2293eb03051a6c2201599d9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-31 16:38:16 -0500

    Add doc strings

--- cam2_imagedepthmaps/utils/version.py
+++ cam2_imagedepthmaps/utils/version.py
@@ -1,4 +1,10 @@
 version: str = "0.0.1"
 
 def __version__()   -> str:
+    """
+    __version__ returns the version of the application.
+
+    :return: The version of the application.
+    :rtype: str
+    """
     return version
