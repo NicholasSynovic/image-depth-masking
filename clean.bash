84ad4c2dff27be78d1cb9740aac320a70d413cd8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-31 15:43:37 -0500

    Create a cleaner file

--- clean.bash
+++ clean.bash
@@ -12,3 +12,11 @@ poetry run isort $1
 
 # Security
 poetry run bandit --severity-level all --confidence-level all --format txt -o checks/bandit_$1.txt $1
+
+# Static typing
+poetry run mypy $1 > checks/mypy_$1.txt
+
+# Linting
+rm checks/flake8_$1.txt
+poetry run flake8 --output-file checks/flake8_$1.txt --isolated $1
+poetry run pylint --rcfile pylintrc --output checks/pylint_$1.txt -f text $1
