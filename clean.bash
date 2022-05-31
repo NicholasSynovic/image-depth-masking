f728690bac7e440fd2e2d2e7a790e14e1cc7dc63
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-31 16:45:17 -0500

    Update filenames

--- clean.bash
+++ clean.bash
@@ -2,7 +2,7 @@
 
 # This is a tool to clean up a Python file and generate reports
 
-filename=$(basename -- "$fullfile")
+filename=$(basename -- "$1")
 filename="${filename%.*}"
 
 mkdir checks
@@ -13,12 +13,12 @@ poetry run black $1
 poetry run isort $1
 
 # Security
-poetry run bandit --severity-level all --confidence-level all --format txt -o checks/bandit_$1.txt $1
+poetry run bandit --severity-level all --confidence-level all --format txt -o checks/bandit_$filename.txt $1
 
 # Static typing
-poetry run mypy $1 > checks/mypy_$1.txt
+poetry run mypy $1 > checks/mypy_$filename.txt
 
 # Linting
-rm checks/flake8_$1.txt
-poetry run flake8 --output-file checks/flake8_$1.txt --isolated $1
-poetry run pylint --rcfile pylintrc --output checks/pylint_$1.txt -f text $1
+rm checks/flake8_$filename.txt
+poetry run flake8 --output-file checks/flake8_$filename.txt --isolated $1
+poetry run pylint --rcfile pylintrc --output checks/pylint_$filename.txt -f text $1
