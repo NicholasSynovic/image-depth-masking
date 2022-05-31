5853e873993b767a9e0b715555108a655cfc463f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-31 15:09:29 -0500

    Fix torch and lxml dependencies

---
+++ clean.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a tool to clean up a Python file and generate reports
+
+filename=$(basename -- "$fullfile")
+filename="${filename%.*}"
+
+# Code formatters
+poetry run autoflake --in-place --remove-all-unused-imports --remove-duplicate-keys --remove-unused-variables $1
+poetry run black $1
+poetry run isort $1
+
+# Security
+poetry run bandit --severity-level all --confidence-level all --format txt -o checks/bandit_$1.txt $1
