d6503dfe99681d35429a3330251631d0082dbc29
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-06-20 13:55:18 -0500

    Add datasets

---
+++ scripts/downloadCIFAR-10.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+directory="../downloads/CIFAR-10"
+
+mkdir ../downloads
+mkdir $directory
+
+wget -q -O $directory/cifar10.zip --progress=bar --show-progress -c https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
---
+++ scripts/downloadSVHN.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directory="../downloads/SVHN"
+
+mkdir ../downloads
+mkdir $directory
+
+wget -q -O $directory/svhnTrain.zip --progress=bar --show-progress -c http://ufldl.stanford.edu/housenumbers/train.tar.gz
+
+wget -q -O $directory/svhnTest.zip --progress=bar --show-progress -c http://ufldl.stanford.edu/housenumbers/test.tar.gz
+
+wget -q -O $directory/svhnExtra.zip --progress=bar --show-progress -c http://ufldl.stanford.edu/housenumbers/extra.tar.gz
